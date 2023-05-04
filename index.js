d69160e19b98eab0440d974dfac3441ea80f909b
Badhon sarker <badhonsarker700@gmail.com>
2023-05-04 09:02:05 +0600

    chefs json data create

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 const express = require('express')
 var cors = require('cors')
-// const data = require('./data.json')
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5500
 
 
 app.use(cors())
-const allChefData = require("./data.json")
+const allChefData = require("./chefs.json")
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/data', (req, res) => {
-    res.send(allChefData)
+  res.send(allChefData)
+})
+
+app.get('/data/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  console.log(`params ${id}`);
+  const d = allChefData.find(chef => chef.id === id)
+  res.send(d)
+})
+
+              // Recipes data
+
+  const allRecipesData = require("./food.json")
+
+  app.get('/recipesData', (req, res) => {
+    res.send(allRecipesData)
   })
-  
-//   app.get('/data/:id', (req, res) => {
-//       const id = parseInt(req.params.id)
-//       console.log(`params ${id}`);
-//       const d = data.find(job=>job.id===id )||{message: "data not found!"};
-//       res.send(d)
-//   })
-  
+// app.get('/recipesData/:id', (req, res) => {
+//   const id = parseInt(req.params.id)
+//   res.send(recipe)
+// })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
